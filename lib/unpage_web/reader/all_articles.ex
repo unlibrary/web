b3f27ec011091c31b65636c218966aa7af14477f
Robin Boers <robindev2019@outlook.com>
2022-08-27 21:27:13 +0200

    Add read all button

--- lib/unpage_web/reader/all_articles.ex
+++ lib/unpage_web/reader/all_articles.ex
@@ -9,6 +9,7 @@ defmodule UnPageWeb.App.AllArticles do
       socket
       |> assign(:title, "All articles")
       |> assign(:entries, make_call(UnLib.Entries.list_all(user())))
+      |> assign(:show_mark_all_read_button?, true)
 
     {:ok, socket, layout: {UnPageWeb.LayoutView, "reader.html"}}
   end
@@ -16,5 +17,13 @@ defmodule UnPageWeb.App.AllArticles do
   @impl true
   def render(assigns) do
     render(UnPageWeb.ReaderView, "feed.html", assigns)
+  end
+
+  @impl true
+  def handle_event("all-read", _value, socket) do
+    make_call(UnLib.Entries.read_all(user()))
+    entries = UnLib.Entries.list_all(user())
+
+    {:noreply, assign(socket, :entries, entries)}
   end
 end
